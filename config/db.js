8bcb8d537421227295b41e418e7a3e91d51bd421
Prajjwal kapoor <prajjwal.kapoor.mail@gmail.com>
2021-07-06 20:24:55 +0530

    test commit

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const connectDb = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
     });
     if (process.env.NODE_ENV === "development")
-      console.log(`MongoDB Connected ${(await conn).connection.host}`);
+      console.log(`MongoDB Connected ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
   }
