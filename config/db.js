418fa118cff5e6c9f5783ed52f1049a91c83865c
Prajjwal kapoor <prajjwal.kapoor.mail@gmail.com>
2021-07-06 20:56:31 +0530

    test

--- config/db.js
+++ config/db.js
@@ -3,12 +3,15 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://prajjwal:pk12345@cluster0.kljk4.mongodb.net/bootcamperDb?retryWrites=true&w=majority&ssl=true",
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      }
+    );
     if (process.env.NODE_ENV === "development")
       console.log(`MongoDB Connected ${conn.connection.host}`);
   } catch (error) {
